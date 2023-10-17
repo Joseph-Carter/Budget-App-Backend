00f871949b226db83f0e55400e6c660ddf3be043
Joseph-Carter <1joseph1carter@gmail.com>
2023-10-17 19:26:47 -0400

    added creat, read, and delete methods

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express")
-
 const app = express();
+const transactions = require("./controllers/transactionsController")
 
 app.use(express.json())
+app.use(cors())
+
+
+
+app.use("/transactions", transactions)
+
+app.get("/", (req, res) => (
+    res.send(`Welcome to the home page`)
+))
 
 
 module.exports = app;
--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -3,4 +3,33 @@ const express = require("express")
 const transactions = express.Router();
 const transactionsData = require("../models/transactionsModel");
 
+transactions.get("/", (req, res) => {
+    res.json(transactionsData)
+});
+
+transactions.post("/", (req, res) => {
+    const newTransaction = req.body
+    transactionsData.push(newTransaction)
+    res.status(200).json(newTransaction)
+})
+
+transactions.get("/:index", (req, res) => {
+    const { index } = req.params;
+    if(index >= 0 && index < transactionsData.length) {
+        res.status(200).json(transactionsData[index])
+    } else {
+        res.redirect(404)
+    }
+});
+
+transactions.delete("/:arrayIndex", (req, res) => {
+    const arrayIndex = req.params 
+
+    if(transactionsData[arrayIndex]) {
+        const deletedTransaction = transactionsData.splice(arrayIndex, 1)
+        res.status(200).json(deletedTransaction[0])
+    } else {
+        res.status(404).json({error: "Could not locate the transaction trying to be deleted."})
+    }
+});
 
